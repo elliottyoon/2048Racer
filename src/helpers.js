eeb4cb00c52777f0b1d74ac355dadfad5815f33b
Elliott Yoon <elinate91@gmail.com>
2022-06-15 09:29:47 -0400

    Create helpers.js

---
+++ src/helpers.js
@@ -0,0 +1,13 @@
+export findFurthestOpenSpace(index, arr, lendpoint) {
+    // iterates backward through array starting at starting index and looks for 
+    // furthest empty space such that the path to that space is empty
+    let ptr = index - 1;
+    let indexVal = arr[index];
+
+    while (lendpoint < ptr) {
+        if (arr[ptr] == indexVal) return {index: ptr, didMerge: true};
+        if (arr[ptr] !== '') break;
+        ptr -= 1;
+    }
+    return {index: ptr + 1, didMerge: false};
+}
