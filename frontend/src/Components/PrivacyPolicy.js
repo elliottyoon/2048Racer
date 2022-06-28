95cada4b63f369a270142cfd264a041400969fb9
Elliott Yoon <elinate91@gmail.com>
2022-06-27 20:46:07 -0400

    Added routing and privacy policy

---
+++ frontend/src/Components/PrivacyPolicy.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class PrivacyPolicy extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Privacy Policy</h1>
+        <p>Effective date: June 27, 2022</p>
+        <p>We currently do not collect any personal data of any users.</p>
+      </div>
+    );
+  }
+}
+
+export default PrivacyPolicy;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import Board from './Components/Board.js'
+import PrivacyPolicy from './Components/PrivacyPolicy';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
 
 
 
@@ -20,4 +26,15 @@ class Game extends React.Component {
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Game />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+    </Routes>
+  </Router>
+);
+
+function About() {
+  return <h2>About</h2>;
+}
