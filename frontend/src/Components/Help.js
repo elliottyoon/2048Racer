497cd3dba553e7d8282ab1c58bf1aed828e0c6b0
Elliott Yoon <elinate91@gmail.com>
2022-06-29 01:56:10 -0400

    Added why statement :D

--- frontend/src/Components/Help.js
+++ frontend/src/Components/Help.js
@@ -11,7 +11,14 @@ class Help extends React.Component {
           <br></br> <br></br>
           If a tile runs into another tile of the same value, the two will merge into one tile of double value! 
           <br></br><br></br>
-          Try to get the 2048 Tile!</p>
+          Try to get the 2048 Tile!
+        </p>
+        <hr></hr>
+        <h1>Why 4096?</h1>
+      <p>The idea for this game originally came from the spring quarter of my freshman year of college, during which my friend and I got really into racing each other in 2048, sitting next to one another running the original game on each of our own laptops.</p>
+        <p>After the quarter ended, I thought it'd be cool to create my own web app that would allow us to continue the 2048 racing tradition, even with thousands of miles between us. And so, with <b>2</b> (or more!) games of <b>2048</b> running simultaneously, <b>4096</b> was born! 
+        </p>
+        <p>- with love, Elliott</p>
       </div>
     );
   }
